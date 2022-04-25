44c74f78636a1e771ff8eb72c55f41d92edf68f9
SORIK28850 <ncreador@gmail.com>
2022-04-25 16:46:40 +0200

    commmit con ajax

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class HomeController extends AbstractController
+{
+    public function index(): Response
+    {
+        //$url = 'http://feeds.spotahome.com/ads-housinganywhere.json';
+        //$json = file_get_contents($url);
+        //$data = json_decode($json);
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+            'data' => 'data',
+        ]);
+    }
+}
